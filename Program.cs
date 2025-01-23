d2cde9366628c25a4d52a7cd900c8ad0b2309565
falcondev <tinhtcp@gmail.com>
2025-01-23 14:47:54 -0700

    Completed Task 1

--- Program.cs
+++ Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        static void Task1()
+        {
+            // Task 1: Creating variables
+            Console.Write("Please enter a low number: ");
+            int lowNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Please enter a high number (larger than {lowNum}): ");
+            int highNum = Convert.ToInt32(Console.ReadLine());
+
+            int diff = highNum - lowNum;
+
+            Console.WriteLine("The difference between the high number and low number is: " + diff);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task1();
         }
     }
 }
