9c13b48f53f4bc7409484b4f971179fab9577f5c
falcondev <tinhtcp@gmail.com>
2025-01-26 20:55:16 -0700

    Complete all the task

--- Program.cs
+++ Program.cs
@@ -4,75 +4,53 @@ namespace Lab0
 {
     internal class Program
     {
-        static void Task1()
+        static bool IsPrime(double n)
         {
-            // Task 1: Creating variables
-            Console.Write("Please enter a low number: ");
-            int lowNum = Convert.ToInt32(Console.ReadLine());
-            Console.Write($"Please enter a high number (larger than {lowNum}): ");
-            int highNum = Convert.ToInt32(Console.ReadLine());
+            if (n < 2) return false;
+            for(double i = 2; i*i <= n; i++)
+                if(n % i == 0) return false;
 
-            int diff = highNum - lowNum;
-
-            Console.WriteLine("The difference between the high number and low number is: " + diff);
+            return true;
         }
 
-        static void Task2()
+        static double GetUserInput()
         {
-            // Task 2: Looping and Input Validation
-            Console.Write("Please enter a positive low number: ");
-            int lowNum = Convert.ToInt32(Console.ReadLine());
-            while (lowNum < 0)
-            {
-                Console.WriteLine("Invalid number. Please enter again.");
-                Console.Write("Please enter a positive low number: ");
-                lowNum = Convert.ToInt32(Console.ReadLine());
-            }
-
-            Console.Write($"Please enter a high number (larger than {lowNum}): ");
-            int highNum = Convert.ToInt32(Console.ReadLine());
-
-            while (highNum <= lowNum)
-            {
-                Console.WriteLine("Invalid number. Please enter again.");
-                Console.Write($"Please enter a high number (larger than {lowNum}): ");
-                highNum = Convert.ToInt32(Console.ReadLine());
-            }
-
-            Console.WriteLine($"The low number is: {lowNum} | The high number is: {highNum}");
+            double number = Convert.ToInt32(Console.ReadLine());
+            return number;
         }
 
-        static void Task3()
+        static bool ValidateUserInput(double lowNum, double highNum)
         {
-            // Task 3: Using Arrays and File I/O
-            
-            // Copy from Task2 validating input
+            if(lowNum < highNum) return false;
+            if (lowNum < 0 || highNum < 0) return false;
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
             Console.Write("Please enter a positive low number: ");
-            int lowNum = Convert.ToInt32(Console.ReadLine());
-            while (lowNum < 0)
-            {
-                Console.WriteLine("Invalid number. Please enter again.");
-                Console.Write("Please enter a positive low number: ");
-                lowNum = Convert.ToInt32(Console.ReadLine());
-            }
+            double lowNum = GetUserInput();
 
             Console.Write($"Please enter a high number (larger than {lowNum}): ");
-            int highNum = Convert.ToInt32(Console.ReadLine());
+            double highNum = GetUserInput();
 
-            while (highNum <= lowNum)
+            while (!ValidateUserInput(highNum, lowNum))
             {
-                Console.WriteLine("Invalid number. Please enter again.");
+                Console.WriteLine("Invalid low number and high number. Please enter again.");
+                Console.Write("Please enter a positive low number: ");
+                lowNum = GetUserInput();
                 Console.Write($"Please enter a high number (larger than {lowNum}): ");
-                highNum = Convert.ToInt32(Console.ReadLine());
+                highNum = GetUserInput();
             }
 
             // Add the numbers between highNum and lowNum in the array;
-            int n = highNum - lowNum;
-            int[] arr = new int[n-1];
-            int number = lowNum+1;
-            for(int i = 0; i < n-1; i++)
+            int n = (int)highNum - (int)lowNum;
+            List<double> numList = new List<double>();
+            int number = (int)lowNum + 1;
+            for (int i = 0; i < n - 1; i++)
             {
-                arr[i] = number;
+                numList.Add(number);
                 number++;
             }
 
@@ -80,21 +58,38 @@ namespace Lab0
             // Taken from example documentation: https://learn.microsoft.com/en-us/dotnet/api/system.io.file?view=net-9.0
             using (StreamWriter stream = File.CreateText("numbers.txt"))
             {
-                for(int i = n-2; i >= 0; i--)
+                // Write/print out the numbers in the list except the lowNum and highNum
+                // Cause we are only printing/writing out between
+                for (int i = n - 2; i >= 0; i--)
                 {
-                    stream.WriteLine(arr[i]);
-                    Console.WriteLine(arr[i]);
+                    stream.WriteLine(numList[i]);
+                    Console.WriteLine(numList[i]);
                 }
             }
 
             Console.WriteLine("Finish writing the numbers between lowNum and highNum");
-        }
 
-        static void Main(string[] args)
-        {
-            Task1();
-            Task2();
-            Task3();
+            // Read the numbers in the file and adding it into the variable
+            double sum = 0;
+            using (StreamReader stream = File.OpenText("numbers.txt"))
+            {
+                while(!stream.EndOfStream)
+                {
+                    double num = Convert.ToDouble(stream.ReadLine());
+                    sum += num;
+                }
+            }
+
+            Console.WriteLine("The sum of the numbers in the \"numbers.txt\" file is: " + sum);
+
+            // Go through the numbers between highNum and lowNum and check for prime numbers
+            Console.WriteLine($"The prime numbers between {lowNum} and {highNum} is: ");
+
+            for(double i = lowNum+1; i < highNum; i++)
+            {
+                if (IsPrime(i))
+                    Console.WriteLine($"{i} is a prime number");
+            }
         }
     }
 }
